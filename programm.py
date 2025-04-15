e5e00f17278609a1f058a0ff70c53073e50a21cb
Gera <gera51666@gmail.com>
2025-04-15 18:55:24 +0300

    new

--- programm.py
+++ programm.py
@@ -10,40 +10,80 @@ from passlib.hash import ldap_pbkdf2_sha256
 from cryptography.fernet import Fernet
 from schemas import Detail
 import toml
+import os 
+from apscheduler.schedulers.background import BackgroundScheduler
+from typing import Optional
 
-
-
+#инициализация логирования
 start_logging()
 logger =logging.getLogger(__name__)
-with open("config.toml") as f:  
-    data = toml.load(f)
-cipher = Fernet(data['KEY'])
-app = FastAPI()
+
+#открытие конфига для ключа шифрования 
+config_path = os.environ.get('CONFIG_PATH', 'config.toml')
+with open(config_path) as f:  
+    config = toml.load(f)
+encryption_key = os.environ.get('ENCRIPTION_KEY', config['KEY'])
+cipher = Fernet(encryption_key.encode() if isinstance(encryption_key, str) else encryption_key)
+
+#инициализация FastAPI
+app = FastAPI(
+    title='Одноразовые секреты',
+    description='Приложение для хранения секретов',
+    version='1.0.0'
+)
+
+def get_client_ip(request: Request) -> str:
+    if 'x_forwarded_for' in request.headers:
+        return request.headers['x_forwarded_for'].split(",")[0].strip()
+    return request.client.host
+
+
+#шаблон очистки устаревших данных из таблицы
+def shemas_cash(conn = Depends(get_db)):
+    with conn.cursor() as curs:
+        curs.execute('DELETE FROM secrets_users WHERE last_add < NOW() RETURNING id')
+        quanity_id = curs.fetchall() 
+        if quanity_id:
+            logger.info(f'Delete {len(quanity_id)} id(cache)')
+        
+        conn.commit()
+
+#программа очистки устаревших данных 
+def clear_cash():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(shemas_cash, 'interval', minutes=5)
+    scheduler.start()
+    
+
+
+#Добавление папки с шаблонами html 
 templates = Jinja2Templates(directory="templates")
 
 #передача шаблона html 
 @app.get("/", response_class=HTMLResponse)
 async def read_form(request: Request):
+
     return templates.TemplateResponse("form.html", {"request": request})
 
 
-#Form() помогает FastAPI найти данные необходимые, преобразовать в пайтон типы, проверяет валидацию 
+#обрабатываем информацию ползователя и добавляет в бд 
 @app.post('/secret')
-async def add_secret(secret: str = Form(...), passphrase: str = Form(...), conn = Depends(get_db)):
+async def add_secret(secret: str = Form(...), passphrase: Optional[str] = Form(...), request: Request = None, conn = Depends(get_db)):
     try:
-        
-        pass_hash = ldap_pbkdf2_sha256.hash(passphrase)
+        ip_client = get_client_ip(request)
+        logger.info(f'open db for post secret')
+        pass_hash = None
+        if passphrase:
+            pass_hash = ldap_pbkdf2_sha256.hash(passphrase)
         secret_hash = cipher.encrypt(secret.encode()).decode()
         logger.info(f'create secret {secret_hash}')
-        # pwd_context.verify(passphrase, pass_hash)
-        logger.info(f'add {secret} it is {secret_hash}')
         with conn.cursor() as cursor:
             cursor.execute(
-                "INSERT INTO secrets_users (secret, passphrase) VALUES (%s, %s) RETURNING id;",
-                (secret_hash, pass_hash)
+                "INSERT INTO secrets_users (secret, passphrase, ip_client) VALUES (%s, %s, %s) RETURNING id;",
+                (secret_hash, pass_hash, ip_client)
             )
-            
-            new_key = cursor.fetchone()
+            logger.info(f'Add secret_hash in data base')
+            new_key = cursor.fetchone()['id']
             conn.commit()
             return new_key
     except psycopg2.Error as e:
@@ -51,48 +91,60 @@ async def add_secret(secret: str = Form(...), passphrase: str = Form(...), conn 
         raise HTTPException(status_code=400)
     
 
-
+#обработка получения секрета 
 @app.get('/showsecret')
 async def show_secret(secret_key: int = Query(...), conn = Depends(get_db)):
     try:
         with conn.cursor() as cursor:
-            logger.info(f'open bd')
+            logger.info(f'open db for get secret')
             cursor.execute(
-                "SELECT secret FROM secrets_users WHERE id = %s", (secret_key,)
+                "SELECT secret, activite_show FROM secrets_users WHERE id = %s and last_add < NOW()", (secret_key,)
             )
-            new_item = cursor.fetchone()
-            cursor.execute('DELETE FROM secrets_users WHERE id = %s', (secret_key,))
-            conn.commit()
-            logger.info(f'{new_item} this is take out bd')
-            if not new_item:  # Проверяем, есть ли данные
+            secret_dict = cursor.fetchall()
+            logger.info(f'{secret_dict} this is take out database')
+            if not secret_dict: 
                 logger.warning(f'{secret_key} not found with key')
                 raise HTTPException(status_code=404, detail="Секрет не найден")
+            if secret_dict[0]['activite_show']:
+                logger.warning(f'{secret_dict['activite_show']} secret already show')
+                raise HTTPException(status_code=400, detail='this secret already show')
             
-            key = new_item['secret']
-            logger.info(f'{key} this is take KEY, {type(key)} this is type KEY')
+            
+            
+            # if not only_secret or not isinstance(only_secret, str):
+            #         logger.info(f'Is not string {only_secret}')
+            #         raise HTTPException(status_code=400, detail="Not found key")
+            cursor.execute(
+                    """
+                    UPDATE secrets_users 
+                    SET accessed_add = NOW(), activite_show = TRUE
+                    WHERE id = %s
+                    """,
+                    (secret_key,)
+                )
+            conn.commit()
+            only_secret = secret_dict[0]['secret']
             try:
-                if not key or not isinstance(key, str):
-                    logger.info(f' not key {key}')
-                    raise HTTPException(status_code=400, detail="i dont know why")
-                value =  cipher.decrypt(key).decode()
-                logger.info(f'{value} this is last version')
+                original_secret =  cipher.decrypt(only_secret).decode()
+                logger.info(f'end for show secret')
                 
-                return value
+                return original_secret
+            
             except Exception as e:
-                logger.info(f'warnin for secret {key}')
+                logger.info(f'warnin for secret {only_secret}')
                 raise HTTPException(
                     status_code=400,
-                    detail=f"Ошибка при обработке секрета: {str(e), {key}}"
+                    detail=f"Ошибка при обработке секрета: {str(e), {only_secret}}"
                 )       
     except psycopg2.Error as e:
         conn.rollback()
         raise HTTPException(status_code=400)
     
-    
+#обработка удаления секрета по паролю
 @app.post('/delsecret')
 async def delsecret(secret_key: int = Form(...), passphrase: str = Form(...), conn = Depends(get_db)) -> Detail:
     try:
-        logger.info(f'OPEN BD DELETE')
+        logger.info(f'open db for delete secret')
         with conn.cursor() as cursor:
             cursor.execute('SELECT passphrase FROM secrets_users WHERE id = %s', (secret_key,))
             
@@ -100,11 +152,12 @@ async def delsecret(secret_key: int = Form(...), passphrase: str = Form(...), co
             if not password_hash:
                 logger.info(f'Not found passphrase')
                 raise HTTPException(status_code=404, detail="Not found password")
-
+            
             if ldap_pbkdf2_sha256.verify(passphrase, password_hash['passphrase']):
                 cursor.execute('DELETE FROM secrets_users WHERE id = %s', (secret_key,))
                 conn.commit()
-                return HTTPException(status_code=200, detail='password is delete')
+                logger.info(f'delete secret from db')
+                return HTTPException(status_code=200, detail='secret is delete')
             raise HTTPException(status_code=403, detail='Invalid passphrase')
     except ResponseValidationError as e:
         logger.info(f'No hash {password_hash['passphrase']} and {passphrase}')
--- test_py.py
+++
@@ -1,6 +0,0 @@
-import toml 
-
-with open('config.toml') as f:
-    data = toml.load(f)
-
-print(data)
